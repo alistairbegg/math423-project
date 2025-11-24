c7fb390143b98b4bc42554bf8b91faa1869c3df6
Alistair Begg <alistairvbegg@gmail.com>
2025-11-24 16:33:16 -0500

    Fixed

--- project.r
+++ project.r
@@ -5,3 +5,10 @@ b3 <- read.csv(file = file1)
 
 ## Q1
 mult_model <- lm(y ~ x1 + x6, data = b3)
+
+## Q2
+a <- anova(mult_model)
+MSR <- a[1, "Mean Sq"]
+MSE <- a[nrow(a), "Mean Sq"]
+f <- MSR / MSE
+p <- pf(f, 2, df.residual(mult_model), lower.tail = FALSE)
