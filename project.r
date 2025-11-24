16505a3376b85821598905fd01faadb45da9ca56
Alistair Begg <alistairvbegg@gmail.com>
2025-11-24 16:31:03 -0500

    Q1 and Q2

--- project.r
+++ project.r
@@ -1,3 +1,7 @@
+## Load Dataset
 file1 <- paste0("https://raw.githubusercontent.com/",
                 "mcgillstat/regression/main/data/data-table-B3.csv")
 b3 <- read.csv(file = file1)
+
+## Q1
+mult_model <- lm(y ~ x1 + x6, data = b3)
