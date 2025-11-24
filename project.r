089404cfdd43fca3ff6177e1b5a30f6a6015016c
Alistair Begg <alistairvbegg@gmail.com>
2025-11-24 17:06:52 -0500

    Q3

--- project.r
+++ project.r
@@ -7,10 +7,34 @@ b3 <- read.csv(file = file1)
 mult_model <- lm(y ~ x1 + x6, data = b3)
 
 ## Q2
-a <- anova(mult_model)
-s <- summary(mult_model)
-f <- s$fstatistic[1]
-df1 <- s$fstatistic[2]
-df2 <- s$fstatistic[3]
-p <- pf(f, df1 = df1, df2 = df2, lower.tail = FALSE)
+a   <- anova(mult_model); a
+SSR <- sum(a[1:(nrow(a)-1), "Sum Sq"])
+SSE <- a[nrow(a), "Sum Sq"]
+SST <- SSR + SSE
+
+df_reg <- length(coef(mult_model)) - 1
+df_res <- df.residual(mult_model)
+
+MSR <- SSR / df_reg
+MSE <- SSE / df_res
+Fvalue <- MSR / MSE
+pvalue <- pf(Fvalue, df_reg, df_res, lower.tail = FALSE)
+
+data.frame(
+  Source = c("Regression", "Residual", "Total"),
+  DF     = c(df_reg, df_res, df_reg + df_res),
+  SS     = c(SSR, SSE, SST),
+  MS     = c(MSR, MSE, NA),
+  F      = c(Fvalue, NA, NA),
+  p.value= c(pvalue, NA, NA)
+)
+
+## Q3
+simple_model <- lm(y ~ x1, data = b3)
+s_simple <- summary(simple_model)
+r2_mult <- s$r.squared
+adj_r2_mult <- s$adj.r.squared
+r2_mult <- s_simple$r.squared
+adj_r2_mult <- s_simple$adj.r.squared
+
 
